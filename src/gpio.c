698ee5e4055ebf3ef20cdf805a49fdb6023efc22
Ggorets0dev <nikgorets4work@gmail.com>
2026-01-07 00:14:43 +0300

    Extra logging for set pin modes

--- src/gpio.c
+++ src/gpio.c
@@ -1,6 +1,7 @@
 #include "gpio.h"
+#include "log.h"
 
-#define RESET_GPIO_PIN(pin)     digitalWrite(pin, LOW)
+#define RESET_GPIO_PIN(pin)     digitalWrite((pin), LOW)
 
 void set_pins_modes(const GArray* control_zones) {
     for (size_t i = 0; i < control_zones->len; i++) {
@@ -9,6 +10,11 @@ void set_pins_modes(const GArray* control_zones) {
         if (zone->type == CONTROL_ZONE_TYPE_GPIO) {
             pinMode (zone->pin, OUTPUT) ;
             RESET_GPIO_PIN(zone->pin);
+
+            g_log_structured(G_LOG_DOMAIN, G_LOG_LEVEL_MESSAGE,
+                "MESSAGE", "Set OUTPUT mode for request control zone",
+                "CONTROL_ZONE", zone->id,
+                "CONTROL_ZONE_PIN", zone->pin);
         }
     }
 }
